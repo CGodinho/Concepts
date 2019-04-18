5190f040c7ea4a0938ab759c5c459a6950dee210
Carlos Godinho <cma.godinho@gmail.com>
2019-04-18 16:37:11 +0100

    1st upload Dfro DTB

---
+++ DTB_Challange_BigData/code/Consolidation/src/main/scala/com/daimler/consolidation/data/Physician.scala
@@ -0,0 +1,38 @@
+package com.daimler.consolidation.data
+
+import com.daimler.consolidation.Constants
+import com.daimler.consolidation.Field
+import scala.util.matching.Regex
+
+
+/**
+ * This class represents a Physician.
+ * It holds specific fields for each parameter.
+ * Class Physician extends base class Entity.
+ */
+case class Physician (npi : Long, medicalSchool : String) extends Entity
+
+
+/**
+ * Companion object for class Physician.
+ * It holds specific fields for each parameter.
+ * Object Physician extends base object EntityCompanion.
+ */
+object Physician extends EntityCompanion[Physician] {
+
+    final val PosNpi           : Int = 1
+    final val PosMedicalSchool : Int = 10
+
+    def key(record : String , fields : List[Int]) : String = Field.getToken(Physician.PosNpi, record, fields)
+
+    def size : Int = Constants.RecordSizePhysician
+
+    def separator : Char = ','
+
+    def quote : Char = '"'
+
+    def validate : Regex = Constants.RegExValidPhysician
+
+    def factory(record : String , fields : List[Int]) : Entity =  Physician(Field.getToken(Physician.PosNpi,           record, fields).toLong,
+                                                                            Field.getToken(Physician.PosMedicalSchool, record, fields))
+}
