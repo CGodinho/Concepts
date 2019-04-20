aa497eaa5c954dab625d272b9e657645d84ad461
Carlos Godinho <cma.godinho@gmail.com>
2019-04-20 01:17:01 +0100

    Update tests

---
+++ DTB_Challenge_BigData/test/redhat_linux_cluster/execution/run.sh
@@ -0,0 +1,21 @@
+SPARK_HOME="/opt/spark-2.4.0"
+CONSOLIDATION_HOME="/mnt/daimler/consolidation/"
+MASTERNODE="master"
+SPARKPORT="7077"
+CONFIGS="spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/opt/spark-2.4.0/conf/log4j.properties"
+NAMENODE=${MASTERNODE}:${SPARKPORT}
+HADOOP_CONSOLIDATION_HOME="/daimler/consolidation/"
+HADOOP_CONFIG="/opt/hadoop-2.9.2/etc/hadoop/core-site.xml,/opt/hadoop-2.9.2/etc/hadoop/hdfs-site.xml"
+
+$SPARK_HOME/bin/spark-submit \
+    --conf $CONFIGS \
+    --master spark://$NAMENODE \
+    --executor-memory 4G \
+    --driver-memory 8G \
+    $CONSOLIDATION_HOME/consolidation_2.11-*.jar \
+    $HADOOP_CONFIG \
+    $HADOOP_CONSOLIDATION_HOME \
+    PartD_Prescriber_PUF_Drug_Ntl_15.csv \
+    Physician_Compare_National_Downloadable_File_2015.csv \
+    PartD_Prescriber_PUF_NPI_Drug_15.txt \
+    Consolidation.csv
